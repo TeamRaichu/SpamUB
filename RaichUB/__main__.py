c6e4bba9b05a8d98f2f185244f7f36d372baf7d4
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-02-11 16:26:05 +0530

    Kids

--- RaichUB/__main__.py
+++ RaichUB/__main__.py
@@ -1,1 +1,350 @@
 
+#RaichUB By @Sex0eL
+
+import asyncio
+import sys
+from sys import argv
+import glob
+from pathlib import Path
+from RaichUB.utils import load_plugins
+import logging
+from telethon import events
+from . import Sex, Sex2, Sex3, Sex4, Sex5 , Sex6, Sex7, Sex8, Sex9, Sex10, Sex11, Sex12, Sex13, Sex14, Sex15, Sex16, Sex17, Sex18, Sex19, Sex20, Sex21, Sex22, Sex23, Sex24, Sex25, Sex26, Sex27, Sex28, Sex29, Sex30, Sex31, Sex32, Sex33, Sex34, Sex35, Sex36, Sex37, Sex38, Sex39, Sex40
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+
+path = "RaichUB/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+
+print("SexoeL Bot Spam Successfully deployed -!")
+print("Enjoy! Do visit @GurateGang")
+
+if len(argv) not in (1, 3, 4):
+    try:
+        Sex.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex2.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex3.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex4.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex5.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex6.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex7.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex8.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex9.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex10.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex11.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex12.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex13.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex14.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex15.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex16.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex17.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex18.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex19.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex20.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex21.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex22.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex23.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex24.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex25.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex26.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex27.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex28.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex29.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex30.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex31.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex32.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex33.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex34.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex35.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex36.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex37.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex38.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex39.disconnect()
+    except Exception as e:
+        pass
+    try:
+        Sex40.disconnect()
+    except Exception as e:
+        pass
+else:
+    try:
+        Sex.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex2.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex3.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex4.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex5.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex6.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex7.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex8.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex9.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex10.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex11.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex12.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex13.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex14.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex15.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex16.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex17.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex18.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex19.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex20.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex21.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex22.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex23.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex24.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex25.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex26.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex27.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex28.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex29.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex30.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex31.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex32.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex33.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex34.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex35.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex36.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex37.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex38.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex39.run_until_disconnected()
+    except Exception as e:
+        pass
+    try:
+        Sex40.run_until_disconnected()
+    except Exception as e:
+        pass
